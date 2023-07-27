f84a789d6dc25bd221e055eb676898a7dc8b6106
NickSeale <nicholas.a.seale@gmail.com>
2023-07-27 07:14:14 -0700

    Initial Commit

---
+++ dist/index.js
@@ -0,0 +1,72 @@
+// ~~~~~~~~ Import Statements ~~~~~~~~~
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+// ~~~~~~~~~~~~ Variables ~~~~~~~~~~~~
+const tasks = []; // Stores tasks
+let newId = 0; // Id counter
+const PORT = 4000; // Host port
+// ~~~~~~~~ Schema Definition ~~~~~~~~
+const typeDefs = `#graphql
+
+  # Tasks
+  type Task {
+    id: Int,
+    isCompleted: Boolean,
+    description: String!
+  }
+
+  # Query Definitions
+  type Query {
+    getAllTasks: [Task]
+    getActiveTasks: [Task]
+  }
+
+  # Mutation Definitions
+  type Mutation {
+    markTaskComplete(id: Int!): Task
+    createTask(description: String!): Task
+    deleteTask(id: Int!): Task
+  }
+`;
+// ~~~~~~~~~~~~ Resolvers ~~~~~~~~~~~~~
+const resolvers = {
+    Query: {
+        getAllTasks() {
+            return tasks;
+        },
+        getActiveTasks() {
+            return tasks.filter((element) => !element.isCompleted);
+        },
+    },
+    Mutation: {
+        markTaskComplete(_, args) {
+            const currentTask = tasks.find((element) => element.id === args.id);
+            currentTask.isCompleted = true;
+            return currentTask;
+        },
+        createTask(_parent, args) {
+            newId += 1;
+            const newTask = {
+                id: newId,
+                isCompleted: false,
+                description: args.description,
+            };
+            tasks.push(newTask);
+            return newTask;
+        },
+        deleteTask(_, args) {
+            const currentTask = tasks.find((element) => element.id === args.id);
+            tasks.splice(tasks.indexOf(currentTask), 1);
+            return currentTask;
+        },
+    },
+};
+// ~~~~~~~~~~Server Definition ~~~~~~~~~~
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+// ~~~~~~~~~~~ Initiating Server ~~~~~~~~
+const { url } = await startStandaloneServer(server, {
+    listen: { port: PORT },
+});
---
+++ readme.md
@@ -0,0 +1,92 @@
+# LUSH To-Do App
+A technical assessment for LUSH cosmetics completed in July 2023
+
+## Functionality
+The app has 4 functions:
+1. Creating a to-do task
+2. Viewing active to-do’s
+3. Marking a to-do as complete
+4. Deleting to-do’s
+
+## Technologies
+The server was written in TypeScript using the Apollo GraphQL server
+
+## Installation
+1. Clone the repository
+2. Navigate to the project directory: `cd ToDoApp`
+3. Install modules: `npm i`
+4. Start the server: `npm start`
+5. Navigate your browser to the localhost server. The default is `http://localhost:4000/`
+
+## Usage
+### Get All Tasks
+`query {
+  getAllTasks {
+    id
+    isCompleted
+    description    
+  }}`
+
+### Get Active Tasks
+`query{
+  getActiveTasks {
+    id
+    isCompleted
+    description
+  }
+}`
+
+### Create Task
+In the Operation section:
+`mutation($description: String!){
+  createTask(description: $description) {
+    id
+    isCompleted
+    description
+  }
+}`
+
+In the Variables section:
+`{
+    "description": "Write your task here"
+}`
+
+### Mark a Task Complete
+In the Operation section:
+mutation($id: Int){
+  markTaskComplete(id:$id) {
+    id
+    isCompleted
+    description
+  }
+}
+
+In the Variables section:
+`{
+    "id":2
+}`
+
+Replace with the task id to be marked complete
+
+### Delete Task
+In the Operation section:
+`mutation($id:Int){
+  deleteTask(id:$id) {
+    id
+    isCompleted
+    description
+  }
+}`
+
+In the Variables section:
+`{
+    "id":2
+}`
+
+Replace with the task id to be deleted
+
+## Connect with me
+
+ <a href="https://www.linkedin.com/in/nseale/">
+    <img src="https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" />
+ </a>
